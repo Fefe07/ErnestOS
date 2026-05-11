affbe7f880e6b21e83bac5f7530940f48b54c2ec
Dihellgo <92475915+Dihellgo@users.noreply.github.com>
2026-05-11 11:03:17 +0200

    added basic memory management

--- kernel.c
+++ kernel.c
@@ -1,17 +1,21 @@
+#include "pmm.h"
 #include "terminal.h"
+#include "utilities.h"
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
-void init_gdt();
-void init_idt();
-int shell();
+extern void init_gdt();
+extern void init_idt();
+extern int shell();
 
-void kernel_main(void) {
+void kernel_main(multiboot_info_t *mbd, unsigned int magic) {
   terminal_initialize();
   init_gdt();
   init_idt();
+  pmm_init(mbd);
 
   terminal_writestring("Bienvenue sur ErnestOS.\n");
+
   shell();
 }
