7756f6eca54b7db1a7806260057ab8ac53efdb38
Fefe07 <felixlandreau97@gmail.com>
2026-05-11 13:20:38 +0200

    help

--- help.c
+++ help.c
@@ -20,6 +20,47 @@ int help_main(int argc, char** argv){
         return 0;
     }
     else if (argc==2) {
+        if(strcmp(argv[1], "bonjour")){
+            terminal_writestring("Ecrit Hello World\n");
+            return 0 ;
+        }
+        if(strcmp(argv[1], "clear")){
+            terminal_writestring("Efface l'historique des commandes\n");
+            return 0 ;
+        }
+        if(strcmp(argv[1], "echo")){
+            terminal_writestring("Prend 1 argument , ecrit son argument\n");
+            return 0 ;
+        }
+        if(strcmp(argv[1], "exit")){
+            terminal_writestring("Tue le terminal\n");
+            return 0 ;
+        }
+        if(strcmp(argv[1], "help")){
+            terminal_writestring("Module d'aide \n");
+            terminal_writestring("help liste les commandes existantes");
+            terminal_writestring("Tapez help command pour plus de precisions sur une commande\n");
+            return 0 ;
+        }
+        if(strcmp(argv[1], "sleep")){
+            terminal_writestring("La commande sleep prend un argument entier et attend le nombre de ticks correspondant\n");
+            terminal_writestring("Actuellement 18 ticks = 1 seconde\n");
+            return 0 ;
+            /* TODO : A preciser    */
+        }
+        if(strcmp(argv[1], "time")){
+            terminal_writestring("La commande time renvoie le temps en ticks depuis le demarrage\n");
+            terminal_writestring("Actuellement 18 ticks = 1 seconde\n");
+            return 0 ;
+            /* TODO : A preciser    */
+        }
+
+
+        terminal_writestring("Commande ou option inconnue ou non documentee\n");
+        return 1 ;
+    }
+    else{
+        terminal_writestring("too much arguments\n");
         return 1 ;
     }
 }
--- utilities.h
+++ utilities.h
@@ -1,3 +1,5 @@
 #include <stdint.h>
+#include <stdbool.h>
 extern void unsigned_int_to_string(uint32_t x, char *res);
 uint32_t string_to_unsigned_int(char *string);
+extern bool strcmp(char *s1, char *s2);
